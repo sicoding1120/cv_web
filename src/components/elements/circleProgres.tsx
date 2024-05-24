481403cdea8af9c10a2fe33d91a8017f47b6a323
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-05-24 23:15:50 +0700

    size circle

--- src/components/elements/circleProgres.tsx
+++ src/components/elements/circleProgres.tsx
@@ -3,8 +3,8 @@
 
   const CircleProses = ({ proses, className, key, color }: { proses: number; className?: string; key?: number; color: string}) => {
     return (
-      <CircularProgress value={proses} className={className} key={key} size={16} color={color}>
-            <CircularProgressLabel fontSize={16}>{proses}%</CircularProgressLabel>
+      <CircularProgress value={proses} className={className} key={key} size={20} color={color}>
+        <CircularProgressLabel fontSize={20}>{proses}%</CircularProgressLabel>
       </CircularProgress>
     );
   }
